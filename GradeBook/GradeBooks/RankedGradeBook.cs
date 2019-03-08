bfbeafb8324bd7dfb4837d7a7b44021e4cea5094
MLaverton <33700030+MLaverton@users.noreply.github.com>
2019-03-08 14:07:17 +0000

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+
     using GradeBook.Enums;
 
     class RankedGradeBook:BaseGradeBook
@@ -11,6 +14,39 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name):base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var studentCount = Students.Count;
+          
+            if (studentCount < 5)
+            {
+                throw (new InvalidOperationException("Ranked-grading requires a minimum of 5 students"));
+            }
+
+            var thresh = (int)Math.Ceiling(studentCount * 0.2);
+            var studentAveragesList = Students.OrderByDescending(x => x.AverageGrade).Select(l => l.AverageGrade).ToList();
+
+
+            if (studentAveragesList[thresh-1] <= averageGrade)
+            {
+                return 'A';
+            }
+            if (studentAveragesList[thresh*2-1] <= averageGrade)
+            {
+                return 'B';
+            }
+            if (studentAveragesList[thresh * 3 - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            if (studentAveragesList[thresh * 4 - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
