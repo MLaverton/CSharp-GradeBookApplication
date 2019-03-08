7c50d67ca5b3f9e4dd265f2b2de844298305cf0f
MLaverton <33700030+MLaverton@users.noreply.github.com>
2019-03-08 11:57:00 +0000

    Complete module 1

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    using GradeBook.Enums;
+
+    class RankedGradeBook:BaseGradeBook
+    {
+        public RankedGradeBook(string name):base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    using GradeBook.Enums;
+
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name):base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
